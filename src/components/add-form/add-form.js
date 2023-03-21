48c3d1a79660d77b1daa22eca1153c61bb134e41
Kirleg744 <lyogky.kirill@gmail.com>
2023-03-21 13:53:50 +0200

    .

--- src/components/add-form/add-form.js
+++ src/components/add-form/add-form.js
@@ -24,7 +24,7 @@ export default class AddForm extends Component{
 
     render(){
         return(
-            <form onSubmit = {this.onSubmit} className="d-flex">
+            <form onSubmit = {this.onSubmit} className="d-flex add-form">
                 <input value = {this.state.label} onChange={this.onLabelChange} placeholder = "Что нужно сделать?" className="form-control" type="text" />
                 <button className="btn btn-success">Добавить задачу</button>
             </form>
